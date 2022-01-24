cacead19933c1d746dab9a435194c29c17b50933
Naback <naback@live.com>
2022-01-23 22:48:35 -0300

    service base created

---
+++ PokedexBusinessService/src/main/java/br/com/naback/business/PokedexBusinessServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.naback.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexBusinessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexBusinessServiceApplication.class, args);
+	}
+
+}
