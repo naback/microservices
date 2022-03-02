175ec2d749166dbfead1b4d86e80bb4b5d842a1d
Naback <naback@live.com>
2022-03-02 19:52:29 -0300

    data service project base created

---
+++ PokedexDataService/PokedexDataService/src/main/java/org/pokedex/dataservice/PokedexDataServiceApplication.java
@@ -0,0 +1,13 @@
+package org.pokedex.dataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexDataServiceApplication.class, args);
+	}
+
+}
---
+++ PokedexDataService/PokedexDataService/src/test/java/org/pokedex/dataservice/PokedexDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pokedex.dataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
