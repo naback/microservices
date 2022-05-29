6c9beeb7d0ea018b23c26996d23f76b1dbb7e714
Naback <naback@live.com>
2022-05-29 19:13:13 -0300

    several fixes

--- PokedexDataService/src/main/java/org/pokedex/dataservice/controllers/PingController.java
+++ PokedexDataService/src/main/java/org/pokedex/dataservice/controllers/PingController.java
@@ -1,15 +1,20 @@
 package org.pokedex.dataservice.controllers;
 
 import org.pokedex.dataservice.openapi.PingApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PingController implements PingApi
 {
+    private static final Logger log = LoggerFactory.getLogger(PingController.class);
+
     @Override
     public ResponseEntity<String> ping()
     {
+        log.info("people are testing me");
         return ResponseEntity.ok("i'm alive");
     }
 }
